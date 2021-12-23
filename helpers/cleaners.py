5eb6294b6a6bfa350674c95ac3ea1a9a06dc54e6
nitesh1489 <93468716+nitesh1489@users.noreply.github.com>
2021-12-23 18:05:39 +0530

    Add files via upload

--- helpers/cleaners.py
+++ helpers/cleaners.py
@@ -1,25 +1,53 @@
 '''cleaner'''
+
 from typing import Union
+
 import codecs
+
 class TextCleaners:
-    @staticmethod
-    def parse_num_from_string(text:str) -> str:
-        text=text.strip().lower()
-        text=''.join([alpha for alpha in text if alpha.isdigit()])
-        num=int(text)
-        return num
 
     @staticmethod
-    def strip_lines_n_space(text:Union[str,list]) -> str:
-        if isinstance(text,str):
-            clean=text.strip('\n').strip()
-            return clean
-        if isinstance(text,list):
-            clean=' '.join([subtext.strip('\n').strip() for subtext in text])
-            return clean
-        
+
+    def parse_num_from_string(text:str) -> str:
+
+        text=text.strip().lower()
+
+        text=''.join([alpha for alpha in text if alpha.isdigit()])
+
+        num=int(text)
+
+        return num
+
+
+
     @staticmethod
+
+    def strip_lines_n_space(text:Union[str,list]) -> str:
+
+        if isinstance(text,str):
+
+            clean=text.strip('\n').strip()
+
+            return clean
+
+        if isinstance(text,list):
+
+            clean=' '.join([subtext.strip('\n').strip() for subtext in text])
+
+            return clean
+
+        
+
+    @staticmethod
+
     def remove_ascii(text:str):
+
         string=text.encode(encoding = 'UTF-8')
+
         string=codecs.decode(string, 'UTF-8')
+
         return string
+
+
+
+"hello This has been updated"
